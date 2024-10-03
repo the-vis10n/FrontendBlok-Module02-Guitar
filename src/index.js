5c795a7b68b50823b00af63fee2a7e7003c25c8f
Evgeny Belov <belov.e.e@mail.ru>
2024-10-03 18:21:00 +0300

    урок 18

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import './styles/main.scss'
+import './styles/normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.slider-teacher', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    modules: [Navigation, Pagination],
+    
+});
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var panel = this.nextElementSibling;
+   if (panel.style.maxHeight) {
+     panel.style.maxHeight = null;
+   } else {
+     panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+ });
+}
